604ef2483b131c71f37b13ae6708f933c76bc2c1
Justin <justin.huang@sjsu.edu>
2022-02-24 01:06:11 -0800

    remove referenced tables, cleanup comments, spacin

--- Multiplier.lua
+++ Multiplier.lua
@@ -23,54 +23,41 @@ function dump(o)
 -- ------------
 -- Functions
 -- ------------
--- Take a table input, returns a table ouput. Multiplies all values by 0.85
-function EasyMultiplier(m, allPrices)
-    -- local scalingAmount = { 0.85, 1, 1.08, 1.2 }
-    local easyDifficulty = allPrices
-    local mediumDifficulty = allPrices
-    local hardDifficulty = allPrices
-    local impoppableDifficulty = allPrices
-    local difficultyTable = { easyDifficulty, mediumDifficulty, hardDifficulty, impoppableDifficulty }
-    -- print(dump(difficultyTable))
-    -- -- Run the inside code four times. WHY IS IT MULTIPLYING EASYDIFFICULTY FOUR TIMES INSTEAD OF GOING TO THE NEXT INDEX?
-    --for a, difficulty in ipairs(difficultyTable) do
-        -- print(dump(difficultyTable[a]), "this is difficulty table index a")
-        -- print("this is A", a)
-        -- Traverse through the whole array.
-        for i, v in ipairs(allPrices) do -- (replace easyDifficulty with difficultyTable[a]) -- I could do this with 8 for loops for each difficulty, or 3 for loops that contains all four tables, but the latter method isn't working
-            -- if scalingAmount[a] == 1 then break end
-            -- print(dump(difficultyTable[a]),"this is the second for loop")
-            -- Traverse through the array of the array.
+-- Take a table (allPrices) and modifier (m) input, returns a table ouput. Multiplies all values by m
+function Multiplier(m, allPrices)
+        -- Traverse through the whole array (2D), from path 1 to path 3.
+        for i, v in ipairs(allPrices) do
+            -- Traverse through the array of the array (1D), from tier 1 to tier 5.
             for path, value in ipairs(allPrices[i]) do 
                 allPrices[i][path] = value * m
             end
         end
-    --end
-
     return allPrices
 end
 
+-- Loop through Multiplier 4 times to multiply by the scalingAmount for each copy.
 -- Run calculations to give all four tables of different difficulties
-function Give_All_Tables(towerPrices)
+function GiveAllTables(towerPrices)
     local scalingAmount = { 0.85, 1, 1.08, 1.2 }
-    local allTables = { deepcopy(towerPrices), deepcopy(towerPrices), deepcopy(towerPrices), deepcopy(towerPrices) }
+    local allTables = { Deepcopy(towerPrices), Deepcopy(towerPrices), Deepcopy(towerPrices), Deepcopy(towerPrices) }
     local allMatrix = {}
     for k, difficulty in ipairs(scalingAmount) do
-        matrix = EasyMultiplier(difficulty, allTables[k])
+        -- Calculate the price difficulty modifier, and insert it into the allMatrix
+        matrix = Multiplier(difficulty, allTables[k])
         table.insert(allMatrix, matrix)
     end
-
     return allMatrix
 end
 
--- Copies the original table and returns a copy. Does not handle metatables
-function deepcopy(orig)
+-- Copies the original table with its children as deep as possible and returns a copy. Does not handle metatables
+-- Copy is needed because Lua passes by reference, not value 
+function Deepcopy(orig)
     local orig_type = type(orig)
     local copy
     if orig_type == 'table' then
         copy = {}
         for orig_key, orig_value in next, orig, nil do
-            copy[deepcopy(orig_key)] = deepcopy(orig_value)
+            copy[Deepcopy(orig_key)] = Deepcopy(orig_value)
         end
     else
         copy = orig
@@ -78,5 +65,5 @@ function deepcopy(orig)
     return copy
 end
 
--- checks multiplication for 1 -- print(dump(EasyMultiplier(1, prices.DartMonkey)))
-print(dump(Give_All_Tables(prices.DartMonkey)))
+-- print(dump(Multiplier(2, prices.DartMonkey)))
+print(dump(GiveAllTables(prices.DartMonkey)))
